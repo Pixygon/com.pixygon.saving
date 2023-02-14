772d9f5c8f7e6fe719752dbb0eff95c84d683bac
gentlemanSoup <anders.horne@gmail.com>
2023-02-15 00:21:03 +0100

    Update SettingsSaveData.cs

--- Runtime/Dataclasses/SettingsSaveData.cs
+++ Runtime/Dataclasses/SettingsSaveData.cs
@@ -8,12 +8,25 @@ namespace Pixygon.Saving {
         public Vector3 _playerPos;
         public float _playerRot;
         public bool _hasStarted;
-        public string _waxWallet = string.Empty;
-        public string _ethWallet;
-        public string _eosWallet;
-        public string _tezWallet;
-        public string _polygonWallet;
-        public string _solanaWallet;
-        public string _flowWallet;
+        public bool _isLoggedIn;
+        public AccountData _user;
+    }
+    
+    [Serializable]
+    public class AccountData {
+        public string _id;
+        public string userName;
+        //public string password;
+        public string email;
+        public string picturePath;
+        public string[] friends;
+        public string waxWallet;
+        public string ethWallet;
+        public string tezWallet;
+        public bool artist;
+        public string[] transactions;
+        public string role;
+        public int viewedProfile;
+        public int impressions;
     }
 }
