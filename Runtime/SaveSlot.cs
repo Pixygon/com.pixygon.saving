b877123b85c6ecb948b059756600d7f7856a22ff
gentlemanSoup <anders.horne@gmail.com>
2023-10-29 18:23:13 +0100

    Added saving-stuff

---
+++ Runtime/SaveSlot.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Pixygon.Saving {
+    [Serializable]
+    public class SaveSlot {
+        public bool _isEmpty;
+        public string _dateTime;
+    }
+}
---
+++ Runtime/SaveSlotUI.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+
+namespace Pixygon.Saving {
+    public class SaveSlotUI : MonoBehaviour {
+        public virtual void Set(string slot) { }
+        public virtual void Clear() { }
+    }
+}
